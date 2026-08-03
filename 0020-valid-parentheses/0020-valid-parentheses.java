c400c2503d2e8ccc6afb55bb2895b3b94247edb8
shalinigupta <shalinigupta284560@gmail.com>
2026-08-03 23:59:45 +0530

    Time: 3 ms (86.08%), Space: 43.2 MB (73.68%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stk = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stk.push(ch);
+            } 
+            else if (!stk.isEmpty() &&
+                    ((ch == ')' && stk.peek() == '(') ||
+                     (ch == '}' && stk.peek() == '{') ||
+                     (ch == ']' && stk.peek() == '['))) {
+                stk.pop();
+            } 
+            else {
+                return false;
+            }
+        }
+
+        if (stk.size() > 0) {
+            return false;
+        }
+
+        return true;
+    }
+}
