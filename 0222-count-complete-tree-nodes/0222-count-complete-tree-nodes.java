85b574014e4d1a4301f505d1bf6ec367cd02a117
shalinigupta <shalinigupta284560@gmail.com>
2026-09-02 23:18:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int countNodes(TreeNode root) {
+        int c = 1;
+
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left != null) {
+            c = c + countNodes(root.left);
+        }
+
+        if (root.right != null) {
+            c = c + countNodes(root.right);
+        }
+
+        return c;
+    }
+}
